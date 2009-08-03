e1b1184b02a071cb8fda85f285dff7c8b0b90b96
Donald Plummer <donald@cideasphere.com>
2009-08-03 14:05:15 -0700

    updated cap config.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,17 +2,17 @@ set :application, "arux.net"
 
 set :deploy_to, "/home/arux"
 
-# set :repository, "output/"
-# set :scm, :none
-# set :deploy_via, :copy
-default_run_options[:pty] = true
+set :repository, "output/"
+set :scm, :none
+set :deploy_via, :copy
+#default_run_options[:pty] = true
 
-set :repository,  "git@github.com:dplummer/aruxnet.git"
-set :scm, "git"
+# set :repository,  "git@github.com:dplummer/aruxnet.git"
+# set :scm, "git"
 #set :scm_passphrase, "p@ssw0rd" #This is your custom users password
-set :user, "arux"
-ssh_options[:forward_agent] = true
-set :branch, "master"
+# set :user, "arux"
+# ssh_options[:forward_agent] = true
+# set :branch, "master"
 
 
 

9cfff6b9893acef741f7e7a59bc318b22ce6562c
Donald Plummer <donald@cideasphere.com>
2009-08-03 14:17:17 -0700

    updated cap config.rb with symlink commands

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,3 +21,17 @@ set :use_sudo, false
 role :app, "arux@arux.net"
 role :web, "arux@arux.net"
 role :db,  "arux@arux.net", :primary => true
+
+
+
+namespace(:customs) do
+  task :symlink, :roles => :app do
+    run <<-CMD
+      ln -nfs #{shared_path}/long-term #{release_path}/long-term ;
+      ln -nfs #{shared_path}/cards #{release_path}/images/cards ;
+    CMD
+  end
+end
+
+after "deploy:symlink","customs:symlink"
+after "deploy", "deploy:cleanup"
